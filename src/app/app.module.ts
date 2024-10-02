9ad1e0847123df6cd295198d637e1b745d947b59
faresbrayek2 <faresbrayek2@gmail.com>
2024-10-02 12:02:19 +0100

    Refactor routing and add 404 page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
+import { RouterModule } from '@angular/router';
+import { HighlightDirective } from './Highlight/highlight.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListCategoriesComponent } from './components/list-categories/list-categories.component';
-import { FilterPipe } from './pipes/filter.pipe';
-import { HighlightDirective } from './Highlight/highlight.directive';
 import { ProductsComponentComponent } from './features/product/products-component/products-component.component';
+import { FilterPipe } from './pipes/filter.pipe';
 import { SearchPricePipe } from './pipes/search-price.pipe';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponentComponent },
+  { path: 'products/:id', component: ProductsComponentComponent },
+  { path: 'categories', component: ListCategoriesComponent },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +39,12 @@ import { SearchPricePipe } from './pipes/search-price.pipe';
     ProductsComponentComponent,
     SearchPricePipe,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
